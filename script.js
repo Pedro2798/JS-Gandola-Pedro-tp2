65f529b74864afea2f5bbd11bc055e26f8b065f5
pedro gandola <pedrogandola@hotmail.com>
2023-06-03 16:33:32 -0300

    terminado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("HOLA")
 
 alert("¡Bienvenidos a Imagen Amoblamientos!")
 
-let tipoDePlacard = Number(prompt("Selecione el tipo de placard, hay opción 1 o 2:\n 1 Placard con Frente\n 2 Placard con puerta de abrir \n 3 Interior de Placard solo."))
+let tipoDePlacard = Number(prompt("Selecione el tipo de placard, hay opción 1 o 2:\n 1 Placard con Frente\n 2 Placard con puerta de abrir \n 3 Interior de Placard solo"))
 
 let anchoDePlacard = Number(prompt("Por favor ingrese el ANCHO total del Placard deseado. Trabajamos hasta 2200mm."))
 
@@ -14,17 +14,17 @@ if (anchoDePlacard >= 1 && anchoDePlacard <= 2200){
     }
 }
 
-let altoDePlacard = Number(prompt("Por favor ingrese el ALTO total del Placard deseado. Trabajamos hasta 2650mm."))
+let altoDePlacard = Number(prompt("Por favor ingrese el ALTO total del Placard deseado. Trabajamos hasta desde 1000 a 2650mm."))
 
 if (altoDePlacard >= 1 && altoDePlacard <= 2650){
     alert("El ancho indicado es "+ altoDePlacard)
 }else{
-    while (altoDePlacard=isNaN("") || altoDePlacard > 2650 || altoDePlacard < 0 ) {
+    while (altoDePlacard=isNaN("") || altoDePlacard > 2650 || altoDePlacard < 1000 ) {
         altoDePlacard= Number(prompt("El número ingresado no es válido. Por favor ingrese solo números o un ancho total dentro de los permitidos"))
     }
 }
 
-alert("Todos nuestros placards se distribuyen de la misma manera. Cuentan con dos cajones, sector para colgar y estantes.")
+alert("Todos nuestros placards se distribuyen de la misma manera. Cuentan con dos cajones, sector para colgar y estantes sobre la cajonera.")
 
 
 let precioFrenteDePlacard = 0
@@ -50,3 +50,24 @@ if(tipoDePlacard == 1){
     alert("Su Placard no lleva frente.")
 }
 
+let precioInteriorPlacard = 0
+
+if (anchoDePlacard <= 500 && altoDePlacard <= 1000){
+    precioInteriorPlacard= 30000
+    alert("El valor de su interior de placard es:"+ precioInteriorPlacard)
+}else if ((anchoDePlacard >= 501 && anchoDePlacard <= 1700) && altoDePlacard <= 2000){
+    precioInteriorPlacard= 75000
+    alert("El valor de su interior de placard es:"+ precioInteriorPlacard)
+}else if((anchoDePlacard >= 1701 && anchoDePlacard <= 2200) && altoDePlacard <= 2650){
+    precioInteriorPlacard= 100000
+    alert("El valor de su interior de placard es:"+ precioInteriorPlacard)
+}
+
+function SumaTotal(precioFrenteDePlacard, precioInteriorPlacard){
+
+    return alert("La cotización final de su placard es de:\n"+(precioFrenteDePlacard + precioInteriorPlacard))
+}
+
+SumaTotal(precioFrenteDePlacard, precioInteriorPlacard)
+
+alert("Gracias por visitar nuestro cotizador online. Cualquier consulta comuníquese con Imagen Amoblamientos!")
