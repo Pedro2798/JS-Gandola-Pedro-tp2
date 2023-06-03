14db0bfda5a5827bbb3f2924ed9ac1aee905b001
pedro gandola <pedrogandola@hotmail.com>
2023-06-03 14:07:09 -0300

    Inicio Proyecto

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("HOLA")
+
+alert("Bienvenidos! Ahora le solicitaremos datos necesarios para su login")
+
+let nombreIngresado=prompt("Ingrese su Nombre")
+console.log(nombreIngresado.toLowerCase())
+
+let apellidoIngresado= prompt("Ingrese su Apellido")
+console.log(apellidoIngresado.toLowerCase())
+
+let edad = Number(prompt("Ingrese su Edad"))
+console.log(edad)
+
+
+
