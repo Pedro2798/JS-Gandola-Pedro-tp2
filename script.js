a344176981e658f31c47911f39060a7b9889a2e3
pedro gandola <pedrogandola@hotmail.com>
2023-06-03 15:28:36 -0300

    Guardado por break

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 console.log("HOLA")
 
-alert("Bienvenidos! Ahora le solicitaremos datos necesarios para su login")
+alert("¡Bienvenidos a Imagen Amoblamientos!")
 
-let nombreIngresado=prompt("Ingrese su Nombre")
-console.log(nombreIngresado.toLowerCase())
+let tipoDePlacard = Number(prompt("Selecione el tipo de placard, hay opción 1 o 2:\n 1 Placard con Frente\n 2 Interior de Placard solo."))
 
-let apellidoIngresado= prompt("Ingrese su Apellido")
-console.log(apellidoIngresado.toLowerCase())
+let anchoDePlacard = Number(prompt("Por favor ingrese el ANCHO total del Placard deseado. Trabajamos hasta 2200mm."))
 
-let edad = Number(prompt("Ingrese su Edad"))
-console.log(edad)
+while ((anchoDePlacard >= 0) || (anchoDePlacard <= 2200)){
+
+}
 
 
+let altoDePlacard = Number(prompt("Por favor ingrese el ALTO total del Placard deseado. Trabajamos hasta 2650mm."))
+
+alert("Todos nuestros placards se distribuyen de la misma manera. Cuentan con dos cajones, sector para colgar y estantes.")
+
+
+let precioFrenteDePlacard = 0
+
+if(tipoDePlacard == 1){
+    if(anchoDePlacard <= 1200){
+        precioFrenteDePlacard = 50000
+        alert("El valor del frente de su placard es $50.000-")
+    }else if (anchoDePlacard > 1201 && anchoDePlacard <= 2650){
+        precioFrenteDePlacard = 100000
+        alert("El valor del frente de su placard es $100.000-")
+    }
+} else{
+    precioFrenteDePlacard = 0
+    alert("Su Placard no lleva frente.")
+}
 
